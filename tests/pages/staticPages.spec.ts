ff2f0424848af1dcf75aece62d1937753304e148
blasson42 <brycelasson@gmail.com>
2025-10-07 21:17:34 -0600

    finished the last of the static pages

--- tests/pages/staticPages.spec.ts
+++ tests/pages/staticPages.spec.ts
@@ -1,11 +1,21 @@
 import { test, expect } from 'playwright-test-coverage';
 
 test('load home page', async ({ page }) => {
-    await page.goto('http://localhost:5173/');
+    await page.goto('/');
     await expect(page).toHaveTitle(/JWT/);
 });
 
 test('load history page', async ({ page }) => {
-    await page.goto('http://localhost:5173/history');
+    await page.goto('/history');
     await expect(page.getByRole('heading', { name: /Mama Rucci, my my/i })).toBeVisible();
+});
+
+test('load about page', async ({ page }) => {
+    await page.goto('/about');
+    await expect(page.getByRole('heading', { name: /The secret sauce/i })).toBeVisible();
+});
+
+test('load not found page', async ({ page }) => {
+    await page.goto('/thispagedoesnotexistinmyversionofjwtpizzabutitmightininyoursleejensen');
+    await expect(page.getByRole('heading', { name: /Oops/i })).toBeVisible();
 });
