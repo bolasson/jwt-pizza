91eb2b5c42007cbf2f9726c0c9f34cb9d476b818
blasson42 <brycelasson@gmail.com>
2025-10-16 18:33:11 -0600

    Revert mockOrderRoute

--- tests/mocks/routes/order.route.ts
+++ tests/mocks/routes/order.route.ts
@@ -1,24 +1,15 @@
 import type { Page } from '@playwright/test';
+import { expect } from 'playwright-test-coverage';
 
 export async function mockOrderRoute(page: Page) {
-    await page.route('**/api/order', async (route) => {
-        const method = route.request().method();
-
-        if (method === 'POST') {
-            const body = route.request().postDataJSON();
-            await route.fulfill({ json: { order: { ...body, id: 23 } } });
-            return;
-        }
-
-        if (method === 'GET') {
-            await route.fulfill({ json: { orders: [] } });
-            return;
-        }
-
-        await route.fulfill({ status: 405, json: { message: 'method not allowed' } });
-    });
-
-    await page.route('**/api/orders', async (route) => {
-        await route.fulfill({ json: { orders: [] } });
+    await page.route('*/**/api/order', async (route) => {
+        const orderReq = route.request().postDataJSON();
+        expect(route.request().method()).toBe('POST');
+        await route.fulfill({
+            json: {
+                order: { ...orderReq, id: 23 },
+                jwt: 'atotallylegitimatejwt',
+            },
+        });
     });
 }
