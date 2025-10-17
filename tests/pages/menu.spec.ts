c6fcb88db08b1ea8daa3988c8224bb9c9b595e78
blasson42 <brycelasson@gmail.com>
2025-10-16 20:31:50 -0600

    removed tests for pipeline testing

--- tests/pages/menu.spec.ts
+++ tests/pages/menu.spec.ts
@@ -1,43 +1,43 @@
-import { expect } from 'playwright-test-coverage';
-import { useGeneralMocks } from '../fixtures/base.fixture';
+// import { expect } from 'playwright-test-coverage';
+// import { useGeneralMocks } from '../fixtures/base.fixture';
 
-const test = useGeneralMocks();
+// const test = useGeneralMocks();
 
-test.describe('login and purchase menu item', () => {
-    test('purchase menu item', async ({ page }) => {
-        test.setTimeout(15000);
-        // Go to order page
-        await page.getByRole('button', { name: 'Order now' }).click();
+// test.describe('login and purchase menu item', () => {
+//     test('purchase menu item', async ({ page }) => {
+//         test.setTimeout(15000);
+//         // Go to order page
+//         await page.getByRole('button', { name: 'Order now' }).click();
 
-        // Create order
-        await expect(page.locator('h2')).toContainText('Awesome is a click away');
-        await page.getByRole('combobox').selectOption('4');
-        await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
-        await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
-        await expect(page.locator('form')).toContainText('Selected pizzas: 2');
-        await page.getByRole('button', { name: 'Checkout' }).click();
+//         // Create order
+//         await expect(page.locator('h2')).toContainText('Awesome is a click away');
+//         await page.getByRole('combobox').selectOption('4');
+//         await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+//         await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
+//         await expect(page.locator('form')).toContainText('Selected pizzas: 2');
+//         await page.getByRole('button', { name: 'Checkout' }).click();
 
-        await page.getByPlaceholder('Email address').fill('d@jwt.com');
-        await page.getByPlaceholder('Password').fill('a');
-        await page.getByRole('button', { name: 'Login' }).click();
-        await expect(page.getByRole('link', { name: 'KC' })).toBeVisible();
+//         await page.getByPlaceholder('Email address').fill('d@jwt.com');
+//         await page.getByPlaceholder('Password').fill('a');
+//         await page.getByRole('button', { name: 'Login' }).click();
+//         await expect(page.getByRole('link', { name: 'KC' })).toBeVisible();
 
-        // Pay
-        await expect(page.getByRole('main')).toContainText('Send me those 2 pizzas right now!');
-        await expect(page.locator('tbody')).toContainText('Veggie');
-        await expect(page.locator('tbody')).toContainText('Pepperoni');
-        await expect(page.locator('tfoot')).toContainText('0.008 ₿');
-        await page.getByRole('button', { name: 'Pay now' }).click();
+//         // Pay
+//         await expect(page.getByRole('main')).toContainText('Send me those 2 pizzas right now!');
+//         await expect(page.locator('tbody')).toContainText('Veggie');
+//         await expect(page.locator('tbody')).toContainText('Pepperoni');
+//         await expect(page.locator('tfoot')).toContainText('0.008 ₿');
+//         await page.getByRole('button', { name: 'Pay now' }).click();
 
-        // Check balance
-        await expect(page.getByText('0.008')).toBeVisible();
-    });
-});
+//         // Check balance
+//         await expect(page.getByText('0.008')).toBeVisible();
+//     });
+// });
 
 
-test('menu wobble animation', async ({ page }) => {
-    await page.getByRole('button', { name: 'Order now' }).click();
-    await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
-    await page.waitForTimeout(500);
-    await expect(page.getByRole('link', { name: 'Image Description Veggie A' })).not.toHaveClass(/animate-wobble/);
-});
+// test('menu wobble animation', async ({ page }) => {
+//     await page.getByRole('button', { name: 'Order now' }).click();
+//     await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+//     await page.waitForTimeout(500);
+//     await expect(page.getByRole('link', { name: 'Image Description Veggie A' })).not.toHaveClass(/animate-wobble/);
+// });
