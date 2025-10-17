6a1d2b3a98f650a87316a5e0c8f9f8830546f71d
blasson42 <brycelasson@gmail.com>
2025-10-16 19:20:13 -0600

    Seperated login to occur before testing purchase menu item to reduce runtime

--- tests/pages/menu.spec.ts
+++ tests/pages/menu.spec.ts
@@ -3,35 +3,39 @@ import { useGeneralMocks } from '../fixtures/base.fixture';
 
 const test = useGeneralMocks();
 
-test('purchase menu item', async ({ page }) => {
-    // Go to order page
-    await page.getByRole('button', { name: 'Order now' }).click();
+test.describe('login and purchase menu item', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByRole('link', { name: 'Login' }).click();
+        await page.getByPlaceholder('Email address').fill('d@jwt.com');
+        await page.getByPlaceholder('Password').fill('a');
+        await page.getByRole('button', { name: 'Login' }).click();
+        await expect(page.getByRole('link', { name: 'KC' })).toBeVisible();
+    });
 
-    // Create order
-    await expect(page.locator('h2')).toContainText('Awesome is a click away');
-    await page.getByRole('combobox').selectOption('4');
-    await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
-    await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
-    await expect(page.locator('form')).toContainText('Selected pizzas: 2');
-    await page.getByRole('button', { name: 'Checkout' }).click();
+    test('purchase menu item', async ({ page }) => {
+        // Go to order page
+        await page.getByRole('button', { name: 'Order now' }).click();
 
-    // Login
-    await page.getByPlaceholder('Email address').click();
-    await page.getByPlaceholder('Email address').fill('d@jwt.com');
-    await page.getByPlaceholder('Password').fill('a');
-    await page.getByRole('button', { name: 'Login' }).click();
-    await expect(page.getByRole('link', { name: 'KC' })).toBeVisible();
+        // Create order
+        await expect(page.locator('h2')).toContainText('Awesome is a click away');
+        await page.getByRole('combobox').selectOption('4');
+        await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+        await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
+        await expect(page.locator('form')).toContainText('Selected pizzas: 2');
+        await page.getByRole('button', { name: 'Checkout' }).click();
 
-    // Pay
-    await expect(page.getByRole('main')).toContainText('Send me those 2 pizzas right now!');
-    await expect(page.locator('tbody')).toContainText('Veggie');
-    await expect(page.locator('tbody')).toContainText('Pepperoni');
-    await expect(page.locator('tfoot')).toContainText('0.008 ₿');
-    await page.getByRole('button', { name: 'Pay now' }).click();
+        // Pay
+        await expect(page.getByRole('main')).toContainText('Send me those 2 pizzas right now!');
+        await expect(page.locator('tbody')).toContainText('Veggie');
+        await expect(page.locator('tbody')).toContainText('Pepperoni');
+        await expect(page.locator('tfoot')).toContainText('0.008 ₿');
+        await page.getByRole('button', { name: 'Pay now' }).click();
 
-    // Check balance
-    await expect(page.getByText('0.008')).toBeVisible();
+        // Check balance
+        await expect(page.getByText('0.008')).toBeVisible();
+    });
 });
+
 
 test('menu wobble animation', async ({ page }) => {
     await page.getByRole('button', { name: 'Order now' }).click();
